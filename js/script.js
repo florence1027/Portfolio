4122873f2dfe67d6b3001083f22005dd490446fb
FloPC <florencechartrand1027@gmail.com>
2022-11-09 11:44:45 -0500

    setup

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function ouvrirMenu() {
+  document.getElementById("menu").style.width = "250px";
+  document.getElementById("contenu").style.marginLeft = "250px";
+}
+
+function fermerMenu() {
+  document.getElementById("menu").style.width = "0";
+  document.getElementById("contenu").style.marginLeft= "0";
+}
